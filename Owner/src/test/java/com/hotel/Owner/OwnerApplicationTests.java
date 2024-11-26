90c4e5ccd09dda0ee83e94feebda6851ea7dc546
Rishabh <rs515666@gmail.com>
2024-11-26 22:02:42 +0530

    Created project skeleton

---
+++ Owner/src/test/java/com/hotel/Owner/OwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.Owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Receptionist/src/main/java/com/hotel/Receptionist/ReceptionistApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.Receptionist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptionistApplication.class, args);
+	}
+
+}
